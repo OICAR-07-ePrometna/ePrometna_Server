c76458bd40711918a7dfeef2e46170bf230bff09
killi1812 <cvok.fran@gmail.com>
2025-03-20 17:54:56 +0100

    Updated swagger dock

--- docs/docs.go
+++ docs/docs.go
@@ -15,7 +15,7 @@ const docTemplate = `{
     "host": "{{.Host}}",
     "basePath": "{{.BasePath}}",
     "paths": {
-        "/test": {
+        "/test/": {
             "get": {
                 "description": "do ping",
                 "consumes": [
