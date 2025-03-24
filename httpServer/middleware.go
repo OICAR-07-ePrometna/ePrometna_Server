126116817f9ed24e8fb545530f57f33904e410c1
killi1812 <cvok.fran@gmail.com>
2025-03-24 12:45:40 +0100

    cors headers

--- httpServer/middleware.go
+++ httpServer/middleware.go
@@ -36,6 +36,16 @@ func protect() gin.HandlerFunc {
 
 func corsHeader() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// TODO: write hteaders if neaded
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
 	}
 }
