38a7aeabec89fc6e8aa56ce15593bd52dc6aaac6
Matija Lukanić <matija.lukanic@wem.hr>
2025-03-25 12:11:39 +0100

    CORS header modified

--- httpServer/middleware.go
+++ httpServer/middleware.go
@@ -35,8 +35,21 @@ func protect() gin.HandlerFunc {
 }
 
 func corsHeader() gin.HandlerFunc {
+	// Define allowed origins
+	allowedOrigins := map[string]bool{
+		"http://localhost:8090": true,
+		"http://localhost:8080": true,
+		"http://localhost:8081": true,
+	}
+
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		origin := c.Request.Header.Get("Origin")
+
+		// Check if the origin is allowed
+		if allowedOrigins[origin] {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
