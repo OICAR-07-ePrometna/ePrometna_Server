0134bb9ab3934721717bdc19f755a78eb3fa795a
killi1812 <cvok.fran@gmail.com>
2025-04-10 09:52:53 +0200

    vehicleDetailsDto added

---
+++ dto/vehicleDetailsDto.go
@@ -0,0 +1,27 @@
+package dto
+
+import (
+	"ePrometna_Server/model"
+)
+
+// TODO: add more properties
+type VehicleDetailsDto struct {
+	Uuid           string
+	VehicleType    string
+	VehicleModel   string
+	ProductionYear int
+	Registration   string
+	Owner          UserDto
+	Drivers        []UserDto
+	PastOwners     []UserDto
+	// PastRegistratins []RegistrationDto
+}
+
+// ToModel create a model from a dto
+func (dto *VehicleDetailsDto) ToModel() (*model.Vehicle, error) {
+	panic("unimplemented")
+}
+
+func (dto VehicleDetailsDto) FromModel(m *model.Vehicle) VehicleDetailsDto {
+	panic("unimplemented")
+}
