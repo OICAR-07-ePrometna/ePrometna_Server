8f320897a686ffc258c14a821e5b466edd37a23e
rpetric98 <rpetric@algebra.hr>
2025-04-30 15:34:57 +0200

    Refactor LoadConfig function to utilize godotenv for environment variable loading and improve error handling

--- config/load.go
+++ config/load.go
@@ -1,30 +1,76 @@
 package config
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 // LoadConfig loads in program configuration should be a first thing called in the program
 func LoadConfig() error {
 	fmt.Println("Loading configuration")
 
-	data, err := os.ReadFile(_CONFIG_FILE)
-	if err != nil {
+	if err := godotenv.Load(); err != nil {
+		fmt.Printf("Can't load config using real env\n")
+		fmt.Printf("Env load err = %+v\n", err)
+	}
+	if err := loadData(); err != nil {
+		fmt.Printf("Error loading config, err = %+v\n", err)
 		return err
 	}
-	var conf AppConfiguration
-	err = json.Unmarshal(data, &conf)
-	if err != nil {
-		return err
-	}
-	AppConfig = &conf
 
-	fmt.Println("Configuration loaded in successfully")
-	if AppConfig.IsDevelopment {
-		fmt.Printf("AppConfig: %+v\n", AppConfig)
-	}
-
+	fmt.Println("Configuration loaded successfully")
 	return nil
+}
+
+func loadData() error {
+	conf := &AppConfiguration{}
+	conf.IsDevelopment = isDevEnvironment()
+	conf.DbConnection = loadString("DB_CONN")
+	conf.AccessKey = loadString("ACCESS_KEY")
+	conf.RefreshKey = loadString("REFRESH_KEY")
+	conf.Port = loadInt("PORT")
+
+	if conf.AccessKey == "" {
+		return fmt.Errorf("ACCESS_KEY environment variable is required")
+	}
+	if conf.RefreshKey == "" {
+		return fmt.Errorf("REFRESH_KEY environment variable is required")
+	}
+
+	AppConfig = conf
+	return nil
+}
+
+func loadInt(name string) int {
+	rez := os.Getenv(name)
+	if rez == "" {
+		fmt.Printf("Env variable %s is empty\n", name)
+	}
+	num, err := strconv.Atoi(rez)
+	if err != nil {
+		fmt.Printf("Failed to parse int %s, will use default (8080)\n", rez)
+		return 8080
+	}
+
+	return num
+}
+
+func loadString(name string) string {
+	rez := os.Getenv(name)
+	if rez == "" {
+		fmt.Printf("Env variable %s is empty\n", name)
+	}
+	return rez
+}
+
+func isDevEnvironment() bool {
+	name := "APP_ENV"
+	rez := os.Getenv(name)
+	if rez == "" {
+		fmt.Printf("Env variable %s is empty\n", name)
+	}
+	return rez == "development"
 }
