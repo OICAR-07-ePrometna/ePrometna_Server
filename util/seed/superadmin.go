488071eecb2db261135e753af6b0ec36517fd4b7
killi1812 <cvok.fran@gmail.com>
2025-05-28 19:27:43 +0200

    Seed superadmin fixes

--- util/seed/superadmin.go
+++ util/seed/superadmin.go
@@ -11,33 +11,42 @@ import (
 )
 
 const (
-	_NAME = "SUPERADMIN_PASSWORD"
-	_OIB  = "11111111111"
+	_PASSWORD_ENV = "SUPERADMIN_PASSWORD"
+	_OIB          = "11111111111"
 )
 
+// CreateSuperAdmin creates a SuperAdmin user if one doesn't already exist.
+// It reads the password from the SUPERADMIN_PASSWORD environment variable.
+// The function will panic if required environment variables are missing or
+// if user creation fails, as this is critical for application bootstrap.
 func CreateSuperAdmin() {
 	userCrud := service.NewUserCrudService()
 
 	// Check if SuperAdmin exists
 	{
-		user, err := userCrud.GetUserByOIB(_OIB)
+		_, err := userCrud.GetUserByOIB(_OIB)
 		if err != nil {
 			if errors.Is(err, gorm.ErrRecordNotFound) {
-				zap.S().Errorf("SuperAdmin not found, err %+v", err)
+				zap.S().Infof("SuperAdmin not found, err %+v", err)
 			} else {
 				zap.S().DPanicf("Error reading users, err %+v", err)
 				return
 			}
-		} else if user.OIB == _OIB {
+		} else {
 			zap.S().Infoln("SuperAdmin found")
 			zap.S().Infoln("Skipping superadmin creation")
 			return
 		}
 	}
 
-	password := os.Getenv(_NAME)
+	zap.S().Infoln("Crating superadmin creation")
+	password := os.Getenv(_PASSWORD_ENV)
 	if password == "" {
-		zap.S().DPanicf("Env variable %s is empty\n", _NAME)
+		zap.S().DPanicf("Env variable %s is empty\n", _PASSWORD_ENV)
+		return
+	}
+	if len(password) < 8 {
+		zap.S().DPanicln("SuperAdmin password must be at least 8 characters long")
 		return
 	}
 
@@ -58,7 +67,7 @@ func CreateSuperAdmin() {
 
 	user, err := userCrud.Create(newUser, dto.Password)
 	if err != nil {
-		zap.S().DPanicf("Failed to crate superadmin, err = %+v\n", err)
+		zap.S().DPanicf("Failed to create superadmin, err = %+v\n", err)
 		return
 	}
 	zap.S().Infof("superadmin created, %+v\n", user)
